10747183ead2eb0c723e28178e9c2e01bae7bb85
Sergei Timofeev <se.timofeev@icloud.com>
2023-04-15 20:52:37 +0300

    sql script

--- src/main/java/ru/planetnails/partnerslk/controller/authentication/AuthenticationRestControllerV1.java
+++ src/main/java/ru/planetnails/partnerslk/controller/authentication/AuthenticationRestControllerV1.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 //import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
