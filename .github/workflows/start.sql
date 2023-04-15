f673e2c6da4e3141bf3967924f5d343c8329209e
Sergei Timofeev <se.timofeev@icloud.com>
2023-04-15 17:52:22 +0300

    sql script

--- .github/workflows/start.sql
+++ .github/workflows/start.sql
@@ -2,7 +2,7 @@
 insert into users (id,name,password,status)
 values ('91ae04bc-6a19-47cf-b066-4cf2692d8213',
         'test_user',
-        '$2a$10$lMOafQevsnvtAbSZKi6dyuCORSuD9fic/uLBPYRps6El2bgqOBF1u','ACTIVE')
+        '$2a$10$dKNOh0hMY2MJAr1Y.kq/5.a5HYy5X3bDxMJbmvLuEeR3MF5Pq7zfK','ACTIVE')
 
 insert into roles (name)
 values ('ADMIN')
