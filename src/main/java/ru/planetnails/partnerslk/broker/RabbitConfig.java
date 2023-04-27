ec69d65deaa1d0fcc63d08214a04a3b9ca3b4de4
AlexMAK1 <99129985+AlexMAK1@users.noreply.github.com>
2023-04-27 16:37:15 -0300

    Fixed problem with RabbitMQConsumer.

--- src/main/java/ru/planetnails/partnerslk/broker/RabbitConfig.java
+++ src/main/java/ru/planetnails/partnerslk/broker/RabbitConfig.java
@@ -7,12 +7,9 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.planetnails.partnerslk.service.OrderService;
 
 @Slf4j
 @Configuration
