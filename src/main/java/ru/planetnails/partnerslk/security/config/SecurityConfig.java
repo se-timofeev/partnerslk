128de0151823a72f202b267c9eee5fa601d1a765
Sergei Timofeev <se.timofeev@icloud.com>
2023-12-07 09:59:13 +0300

    add api-docs add swagger multiple security chain

--- src/main/java/ru/planetnails/partnerslk/security/config/SecurityConfig.java
+++ src/main/java/ru/planetnails/partnerslk/security/config/SecurityConfig.java
@@ -1,43 +1,62 @@
 package ru.planetnails.partnerslk.security.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ru.planetnails.partnerslk.security.jwt.JwtTokenFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
-public class SecurityConfig {
-    private final JwtTokenFilter jwtTokenFilter;
-    private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
-    private static final String SWAGGER_WHITELIST1 = "/v3/api-docs/";
-    private static final String SWAGGER_WHITELIST = "/swagger-ui/**";
 
-    private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain configureSwagger(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/v1/auth/login").permitAll()
-                        .requestMatchers("/swagger-ui/index.html").permitAll()
-                        .anyRequest().authenticated()
-                );
+                .securityMatcher("/swagger-ui/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
         return http.build();
-
-
     }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain configureApiDocs(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api-docs/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
 }
